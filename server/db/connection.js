ea8a847c8ca40d6a8851bda2db6de5960a7081aa
lktec-io <lktecio@gmail.com>
2026-07-06 13:09:07 +0300

    ---------------

--- server/db/connection.js
+++ server/db/connection.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = mysql.createPool({
-  host:            process.env.DB_HOST     || 'localhost',
+  host:            process.env.DB_HOST     || '127.0.0.1',
   port:            parseInt(process.env.DB_PORT) || 3306,
   user:            process.env.DB_USER     || 'clix_user',
   password:        process.env.DB_PASSWORD || '',
